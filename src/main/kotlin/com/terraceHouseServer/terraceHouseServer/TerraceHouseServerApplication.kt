7b4c849a14c75ff84c754b092f3418e6809a330f
Dan Cooper <iamdanielcooper@users.noreply.github.com>
2022-05-29 12:08:27 +0100

    Init with Spring & Postgres

---
+++ src/main/kotlin/com/terraceHouseServer/terraceHouseServer/TerraceHouseServerApplication.kt
@@ -0,0 +1,11 @@
+package com.terraceHouseServer.terraceHouseServer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TerraceHouseServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<TerraceHouseServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/terraceHouseServer/terraceHouseServer/TerraceHouseServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.terraceHouseServer.terraceHouseServer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TerraceHouseServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
